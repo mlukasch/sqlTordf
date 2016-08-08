a594235b6e9b120c22d77bc9bcc10dd824d554fc
Maik Lukasch <mlukasch@gmail.com>
2016-08-08 18:31:28 +0200

    add

--- src/main/java/de/fhg/ids/iais/ldcontainer/controller/RestController.java
+++
@@ -1,7 +0,0 @@
-package de.fhg.ids.iais.ldcontainer.controller;
-
-/**
- * Created by maik on 04.08.16.
- */
-public class RestController {
-}
--- src/main/java/de/fhg/ids/iais/ldcontainer/service/LinkedDatasetService.java
+++
@@ -1,7 +0,0 @@
-package de.fhg.ids.iais.ldcontainer.service;
-
-/**
- * Created by maik on 04.08.16.
- */
-public class LinkedDatasetService {
-}
